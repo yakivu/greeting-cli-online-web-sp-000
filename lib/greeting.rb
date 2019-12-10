4010d2947e76489ada225c7c04a97989f977dda2
Adam Walker <adamw207@yahoo.com>
2019-12-10 19:39:09 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,29 @@
-# code the #greeting method here!
+# code the #greeting method here!RSpec.configure do |config|
+  config.order = :default
+end
+
+def run_file(file)
+  eval(File.read(file), binding)
+end
+
+def get_variable_from_file(file, variable)
+  file_scope = binding
+  file_scope.eval(File.read(file))
+  
+  begin
+    return file_scope.local_variable_get(variable)
+  rescue NameError
+    raise NameError, "local variable `#{variable}' not defined in #{file}."
+  end    
+end
+
+def capture_puts
+  begin
+    old_stdout = $stdout
+    $stdout = StringIO.new('','w')
+    yield
+    $stdout.string
+  ensure
+    $stdout = old_stdout
+  end
+end
